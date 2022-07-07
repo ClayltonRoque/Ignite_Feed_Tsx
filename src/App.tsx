710561d2d4e5ffea3c6a16e658bf161a7b3403e0
Claylton Roque <claylton202@gmail.com>
2022-07-06 22:50:24 -0300

    nova patch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Header } from './components/Header'
 import { Post } from './components/Post'
 import { Sidebar } from './components/Sidebar'
-import img from './assets/lukas.jpg'
+
 
 import styles from './App.module.css'
 import './Global.css'
@@ -37,45 +37,31 @@ const posts : AllProps[] = [
   {
     id: 1,
     author: {
-      avatarUrl: "https://i.pinimg.com/originals/1a/cf/2c/1acf2cf477f133344418dc46e3fa8ae8.gif",
+      avatarUrl: "https://i.pinimg.com/originals/72/6e/5a/726e5ab4a197fb579eac39575e801ac6.gif",
       name: "Claylton Roque",
-      role: "mito"
+      role: "Estudante"
     },
     content: [
-      { type: "paraghaph", content: "komi-san is beautiful" },
-      { type: "paraghaph", content: "aharen-san is great" },
-      { type: "link", content: "kaguya-sama is awesome" },   
+      { type: "paraghaph", content: "Estou aprendendo a usar React e a fazer deploy com o vercel." },
+      { type: "paraghaph", content: "é muito massa, upei um no link:" },
+      { type: "link", content: "https://to-do-mu-three.vercel.app" },   
     ] ,
     publishedAt: new Date('2022-06-20 18:10:00')
   },
   {
     id: 2,
     author: {
-      avatarUrl: "http://pa1.narvii.com/6449/e931e9b09b80f82037375b1e6e59e2685ea55bdd_00.gif",
+      avatarUrl: "https://img.wattpad.com/2e09146f8bcc94885035c2b7e20f3425ac5055cb/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f503063673173484a7249787076513d3d2d3732303534313538332e313539363234666339316330363165643238363132323637373239312e676966",
       name: "Roque",
-      role: "trabaiador"
+      role: "Estudante"
     },
     content: [
-      { type: "paraghaph", content: "komi-san is beautiful ++" },
-      { type: "paraghaph", content: "aharen-san is great ++" },
-      { type: "link", content: "kaguya-sama is awesome ++" },
+      { type: "paraghaph", content: "Estou começando uma nova cadeira na faculdade onde programo em java" },
+      { type: "paraghaph", content: "java parece ser um javaScript tipado, parece legal."  },
+      { type: "link", content: "One piece é bom demais" },
     ],
     publishedAt: new Date('2022-06-20 18:10:00')
   },
-  {
-    id: 3,
-    author: {
-      avatarUrl: "https://s2.glbimg.com/ESHNxNeeVocMXeikWbf2hpD47m4=/0x0:1024x682/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2018/p/6/6xsvJ6SAAT65d9bIsIIA/whatsapp-image-2018-12-01-at-20.40.01-1-.jpeg",
-      name: "Lukinhas",
-      role: "LukasPaquetar"
-    },
-    content: [
-      { type: "paraghaph", content: "tirei 10 em matematica" },
-      { type: "paraghaph", content: "sou ruim em fall guys" },
-      { type: "link", content: "sou ruim em free fire" },
-    ],
-    publishedAt: new Date('2022-06-20 18:10:00')
-  }
 ]
 
 
@@ -100,3 +86,17 @@ export function App() {
     </div>
   )
 }
+
+
+const aluno = [
+  {
+    id: 1,
+    number:5,
+    nome:"a",
+  },
+  {
+    id: 2,
+    number:5,
+    nome:"b",
+  },
+]
